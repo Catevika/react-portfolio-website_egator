e01475ee2ecbea2e3c4050b1eec64cb6f229c956
Catevika <dominique.bello@outlook.fr>
2022-09-01 17:48:03 +0200

    Add components folders & jsx + css files

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,7 @@
+import './Portfolio.css';
+
+const Portfolio = () => {
+	return <div>Portfolio</div>;
+};
+
+export default Portfolio;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,7 @@
+import './Services.css';
+
+const Services = () => {
+	return <div>Services</div>;
+};
+
+export default Services;

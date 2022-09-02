1e2dd3ba73c36a7dd1b743f4c6b634e30540c131
Catevika <dominique.bello@outlook.fr>
2022-09-02 12:59:01 +0200

    Add Header + CSS, CTA, HeaderSocials + react-icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,26 @@
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 import './Header.css';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className='container header__container'>
+				<h5>Hello, I'm</h5>
+				<h1>Dominique Bello</h1>
+				<h5 className='text-light'>Fullstack Developer</h5>
+				<CTA />
+				<HeaderSocials />
+				<div className='me'>
+					<img src={ME} alt='me' />
+				</div>
+				<a href='#contact' className='scroll__down'>
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { BsDribbble } from 'react-icons/bs';
+
+const HeaderSocials = () => {
+	return (
+		<div className='header__socials'>
+			<a href='https://linkedin.com/' target='_blank' rel='noopener noreferrer'>
+				<BsLinkedin />
+			</a>
+			<a href='https://github.com/' target='_blank' rel='noopener noreferrer'>
+				<BsGithub />
+			</a>
+			<a href='https://dribble.com/' target='_blank' rel='noopener noreferrer'>
+				<BsDribbble />
+			</a>
+		</div>
+	);
+};
+
+export default HeaderSocials;

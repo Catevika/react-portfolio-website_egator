af027897d55968b58fbdf1ef056ccfb8249ebddc
Catevika <dominique.bello@outlook.fr>
2022-09-03 17:24:46 +0200

    Add Portfolio + CSS

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,89 @@
+import IMG from '../../assets/Remix-support-desk app.jpeg';
 import './Portfolio.css';
 
+const data = [
+	{
+		id: '1',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	},
+	{
+		id: '2',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	},
+	{
+		id: '3',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	},
+	{
+		id: '4',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	},
+	{
+		id: '5',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	},
+	{
+		id: '6',
+		image: IMG,
+		title: 'Remix Support-Desk App',
+		github: 'https://github.com/Catevika/Remix_support-desk_update_react18',
+		demo: 'https://remix-support-desk-update-react18.vercel.app/'
+	}
+];
+
 const Portfolio = () => {
-	return <section id='portfolio'>Portfolio</section>;
+	return (
+		<section id='portfolio'>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+
+			<div className='container portfolio__container'>
+				{data.map(({ id, image, title, github, demo }) => {
+					return (
+						<article key={id} className='portfolio__item'>
+							<div className='portfolio__item-image'>
+								<img src={image} alt='portfolio' />
+							</div>
+							<h3>{title}</h3>
+							<div className='portfolio__item-cta'>
+								<a
+									href={github}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='btn'
+								>
+									Github
+								</a>
+								<a
+									href={demo}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='btn btn-primary'
+								>
+									Live Demo
+								</a>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
